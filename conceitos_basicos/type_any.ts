d58289ab9d04054dd4db4ab33e5a9fcad4c14991
eduardotrandafilov <eduardo.trandafilov@gmail.com>
2021-07-05 13:41:32 -0300

    feat: type any

---
+++ conceitos_basicos/type_any.ts
@@ -0,0 +1,7 @@
+/* type any é específico apenas para typescript e informa que queremos 
+utilizar uma variável de um tipo qualquer para receber nossos dados
+*/
+
+let nomes: any = ['eduardo','danilo','nadia','fulano',40];
+
+nomes.push(true);
