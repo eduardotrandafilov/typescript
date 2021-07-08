84c6f2734fdacffb267741ce26c27b62a734633d
eduardotrandafilov <eduardo.trandafilov@gmail.com>
2021-07-08 17:17:30 -0300

    feat: diretivas noUnusedLocals, noUnusedParameters, noImplicitReturns

---
+++ configuracoes_ts/src/libs/outro.ts
@@ -0,0 +1,14 @@
+function somar(n1:number,n2:number,n3?:number):number{
+    //let nome = "eduardo";
+    return n1 + n2;
+}
+
+function verificarPar(n1:number):boolean {
+    if(n1%2 === 0 ){
+        return true;
+    }else{
+        //return false;
+    }
+
+}
+somar(1,2);
